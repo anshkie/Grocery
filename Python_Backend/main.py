365047c634b834cd7b7f22cbeb8febc1fd002527
anshkie <anshgup444@gmail.com>
2025-05-17 02:28:10 +0530

    changes

--- Python_Backend/main.py
+++ Python_Backend/main.py
@@ -10,7 +10,11 @@ from fastapi.responses import JSONResponse
 import stripe
 import os
 app = FastAPI(title="Grocery Store API", description="API for Grocery Store", version="1.0.0")
+port = int(os.environ.get("PORT", 8000))
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 # CORS setup
 origins = [
     "http://localhost",
