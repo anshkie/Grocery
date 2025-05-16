7226471d73336ed111b3c0bd18e8eb68f8a0de06
anshkie <anshgup444@gmail.com>
2025-05-17 03:38:12 +0530

    commit

--- Python_Backend/database.py
+++ Python_Backend/database.py
@@ -1,24 +1,46 @@
+# import mysql.connector
+# from mysql.connector import errorcode
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# def get_db_connection():
+#     try:
+#         cnx = mysql.connector.connect(
+#             user=os.getenv('DB_USER'),
+#             password=os.getenv('DB_PASSWORD'),
+#             host=os.getenv('DB_HOST'),
+#             database=('GroceryStore')
+#         )
+#         return cnx
+#     except mysql.connector.Error as err:
+#         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#             print("Something is wrong with your user name or password")
+#         elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#             print("Database does not exist",err)
+#         else:
+#             print(err)
+#     return None
+
+
 import mysql.connector
-from mysql.connector import errorcode
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Load variables from .env if running locally
 
 def get_db_connection():
     try:
-        cnx = mysql.connector.connect(
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            host=os.getenv('DB_HOST'),
-            database=('GroceryStore')
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("PORT", 4000)),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            ssl_ca=os.path.join(os.path.dirname(__file__), "isegrootx1.pem")  # Path to your CA cert
         )
-        return cnx
+        return connection
     except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist",err)
-        else:
-            print(err)
-    return None
+        print("Database connection error:", err)
+        return None
--- Python_Backend/main.py
+++ Python_Backend/main.py
@@ -10,7 +10,7 @@ from fastapi.responses import JSONResponse
 import stripe
 import os
 app = FastAPI(title="Grocery Store API", description="API for Grocery Store", version="1.0.0")
-port = int(os.environ.get("PORT", 8000))
+port = int(os.environ.get("PORT", 4000))
 
 if __name__ == "__main__":
     import uvicorn
